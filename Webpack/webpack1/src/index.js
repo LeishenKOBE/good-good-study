5f60a4658fc069f9d2b7b0c63f3c9097e0cc39a7
shidongzhao <wuyujiesong@outlook.com>
2022-04-21 23:51:03 +0800

    chore: 学习

---
+++ Webpack/webpack1/src/index.js
@@ -0,0 +1,4 @@
+// import { square } from "./js/util.js";
+const fun = require("./js/util.js");
+
+console.log("square", fun.square(20), fun.sum(10, 10));
---
+++ Webpack/webpack1/src/js/util.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+const square = (a) => a * a;
+
+// export { sum, square };
+
+module.exports = {
+  square,
+  sum,
+};
