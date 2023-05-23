08355f6dead7f3c4cc596893013aee2c2a57a490
LeishenKOBE <wuyujiesong@outlook.com>
2023-05-23 12:12:06 +0800

    init

---
+++ 工程化/webpack5-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "@/app.css";
+import "@/app.less";
+
+function App() {
+  return (
+    <>
+      <div className="smallImg"></div> {/* 小图片背景容器 */}
+      <div className="bigImg"></div> {/* 大图片背景容器 */}
+    </>
+  );
+}
+export default App;
